7a303425e274ba22fe474903cddd2fac7278c5f5
yaprincev <5489812@mail.ru>
2023-08-01 10:29:49 +0300

    init project

---
+++ SummerSchool/SummerSchool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SummerSchool
+//
+//  Created by Максим Япринцев on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

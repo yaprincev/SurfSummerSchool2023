e7856a3f049a2b221db95594f08fe6de7fb4fa8c
yaprincev <5489812@mail.ru>
2023-08-01 14:29:22 +0300

    add fio and info labels

--- SummerSchool/SummerSchool/MainModule/View/MainViewController.swift
+++ SummerSchool/SummerSchool/MainModule/View/MainViewController.swift
@@ -13,6 +13,9 @@ class MainViewController: UIViewController {
     var person: Person!
     
     @IBOutlet weak var personImage: UIImageView!
+    @IBOutlet weak var fioLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +32,17 @@ extension MainViewController: MainViewProtocol {
 private extension MainViewController {
     func configureApperance() {
         self.presenter.showPersonInfo()
+        
+        view.backgroundColor = UIColor(red: 0xF3/255.0, green: 0xF3/255.0, blue: 0xF5/255.0, alpha: 1.0)
+        
         personImage.image = person.image
         
+        fioLabel.text = person.lastName + " " + person.firstName + " " + person.patronymic
+        fioLabel.textColor = UIColor(red: 0x31/255.0, green: 0x31/255.0, blue: 0x31/255.0, alpha: 1.0)
+        fioLabel.textAlignment = .center
+        fioLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        fioLabel.numberOfLines = 0
+        
+
     }
 }

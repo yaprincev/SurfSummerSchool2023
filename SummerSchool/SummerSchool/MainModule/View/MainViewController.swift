b55b1b9ca4c8b1a8ef92390e9483ed6377fa8017
yaprincev <5489812@mail.ru>
2023-08-01 16:12:02 +0300

    add scrollView and other labels

--- SummerSchool/SummerSchool/MainModule/View/MainViewController.swift
+++ SummerSchool/SummerSchool/MainModule/View/MainViewController.swift
@@ -7,19 +7,25 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UIScrollViewDelegate {
     
     var presenter: MainViewPresenterProtocol!
     var person: Person!
+
     
     @IBOutlet weak var personImage: UIImageView!
     @IBOutlet weak var fioLabel: UILabel!
     @IBOutlet weak var infoLabel: UILabel!
-    
-    
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var cityImage: UIImageView!
+  //  @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var skillsLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureApperance()
+        configureTopView()
+        configureBottomView()
+      //  configureScrollView()
     }
 }
 
@@ -30,7 +36,20 @@ extension MainViewController: MainViewProtocol {
 }
 
 private extension MainViewController {
-    func configureApperance() {
+//    func configureScrollView() {
+//        scrollView.translatesAutoresizingMaskIntoConstraints = false
+//        scrollView.showsVerticalScrollIndicator = true
+//        scrollView.alwaysBounceVertical = true
+//
+//        NSLayoutConstraint.activate([
+//            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+//            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+//            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+//        ])
+//        view.addSubview(scrollView)
+//    }
+    func configureTopView() {
         self.presenter.showPersonInfo()
         
         view.backgroundColor = UIColor(red: 0xF3/255.0, green: 0xF3/255.0, blue: 0xF5/255.0, alpha: 1.0)
@@ -43,6 +62,27 @@ private extension MainViewController {
         fioLabel.font = UIFont.boldSystemFont(ofSize: 24)
         fioLabel.numberOfLines = 0
         
-
+        infoLabel.text = person.mainInfo
+        infoLabel.textColor = UIColor(red: 0x96/255.0, green: 0x95/255.0, blue: 0x9B/255.0, alpha: 1.0)
+        infoLabel.font = UIFont.systemFont(ofSize: 14)
+        infoLabel.numberOfLines = 2
+        infoLabel.textAlignment = .center
+        
+        cityLabel.text = person.city
+        cityLabel.textAlignment = .center
+        cityLabel.font = UIFont.systemFont(ofSize: 14)
+        cityLabel.textColor = UIColor(red: 0x96/255.0, green: 0x95/255.0, blue: 0x9B/255.0, alpha: 1.0)
+        cityLabel.numberOfLines = 1
+        
+        cityImage.image = UIImage(named: "City Image")
+        
+    }
+    
+    func configureBottomView() {
+        bottomView.backgroundColor = UIColor.white
+        
+        skillsLabel.text = "Мои навыки"
+        skillsLabel.textColor = UIColor(red: 0x31/255.0, green: 0x31/255.0, blue: 0x31/255.0, alpha: 1.0)
+        skillsLabel.font = UIFont.systemFont(ofSize: 16, weight: .medium)
     }
 }
